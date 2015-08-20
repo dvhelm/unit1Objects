88ab59e4e03db868850b084db34d36f2efa379a5
dvhelm <dvhelm@NN123-HA-02.HS.sd203.org>
2015-08-20 14:11:52 -0500

    Did stuff with a turtle

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -6,8 +6,25 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
+        turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(90);
+        turtle.forward(50);
+        turtle.turn(135);
+        turtle.forward(72);
+        Turtle turtle2 = new Turtle(turtleWorld);
+        turtle2.penDown();
+        turtle.forward(100);
+        turtle.turn(120);
+        turtle.forward(100);
+        turtle.turn(90);
+        turtle.forward(100);
+        
+
     }
 }
