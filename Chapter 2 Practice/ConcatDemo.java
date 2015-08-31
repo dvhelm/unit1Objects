c1b163871b1b0880f6fb4db7e8267fc5fea5c728
dvhelm <dvhelm@NN123-HA-02.HS.sd203.org>
2015-08-31 14:13:16 -0500

    Did a color changer and attempted to use the random class

--- Chapter 2 Practice/ConcatDemo.java
+++
@@ -1,5 +0,0 @@
-public class ConcatDemo
-{
-    public static void main(Strong[] args)
-    {
-        String
---
+++ Chapter 2 Practice/Die_Simulator.java
@@ -0,0 +1,8 @@
+
+
+public class Die_Simulator
+{
+   public static void main(String[] args)
+   {
+       Int die = generator.nextInt(6);
+       println(die);
